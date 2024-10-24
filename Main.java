60e50db771fbbbe1c4412773052894ab9bf1aef7
Aitor de Santos Amoros <soyadsa06@gmail.com>
2024-10-24 10:38:47 +0200

    Creacion de las funciones de suelo y paredes

--- Main.java
+++ Main.java
@@ -17,29 +17,111 @@ public class Main {
         //Declaro el BufferedReader para leer datos por consola
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String name;
-        Boolean salir = false;
-        Byte opcion;
+        String name = "";
+        Boolean quit = false;
+        int option = 0;
+
+        String[] pedirNombre = {"Buenos dias, ¿Como te puedo llamar?"};
+        String[] pedirJugar = {"¿A que deseas jugar " + name + "?", "1 = Jugar", "2 = Conf", "3 = Salir"};
+
+        final int RESOLUCION_ALTURA = 30;
+        final int RESOLUCION_ANCHO = 120;
         /* ----- Parte principal ----- */
 
         //Aqui faltaria limpiar la pantalla por consola
-        System.out.println("Buenos dias, ¿Como te puedo llamar?");
+        
+        sueloTecho(RESOLUCION_ANCHO);
+        paredes(RESOLUCION_ALTURA, 14, -1, pedirNombre);
+        //System.out.println("Buenos dias, ¿Como te puedo llamar?");
+        sueloTecho(RESOLUCION_ANCHO);
         name = br.readLine();
-        System.out.println("\nBienvenido " + name);
+        
+        while (!quit) {
+            sueloTecho(120);
+            paredes(RESOLUCION_ALTURA, 14, -1, pedirJugar);
 
-        /*
-        while (!salir) {
-            System.out.println("¿A que deseas jugar?");
-            opcion = br
-            switch (salir) {
-                case value:
+            //System.out.print("¿A que deseas jugar " + name + "?");
+
+            sueloTecho(120);
+            option = Integer.parseInt(br.readLine());
+            switch (option) {
+                case 1:
                     
                     break;
-            
+                case 2:
+                    
+                    break;
+                case 3:
+                    quit = true;     
+                    break;
                 default:
+                    System.out.println("Opcion incorrecta");
                     break;
             }
         }
-        */
+        System.out.println("Hasta otra");
+    }
+
+    public static void sueloTecho(int longitud) {
+        for(int i = longitud; i > 0; i--) {
+            System.out.print("-");
+        }
+        System.out.printf("%n");
+    }
+
+    public static void paredes(int longitud, int fila, int espaciado, String[] frases) {
+
+        //Hay que tener en cuenta el espaciado y el numero de frases
+
+        //Bucle
+        for(int i = 0; i != (longitud-3); i++) {
+
+            System.out.printf("=");
+
+            //
+            if(i == fila) {
+                
+                if (espaciado <= -1){
+                    for(int j = 0; j != ((118 - frases[0].length()) / 2); j++) {
+                        System.out.print(" ");
+                    }
+
+                    System.out.print(frases[0]);
+
+                    //Terminacion de la frase dependiendo si es impar o par
+                    if (frases[0].length() % 2 == 0) {
+                        for(int j = 0; j != (118 - frases[0].length() - espaciado); j++) {
+                            System.out.print(" ");
+                        }
+                    } else { //Si la frase es impar se le sumara 1 para que no se joda
+                        for(int j = 0; j != ((118 - frases[0].length()) / 2 + 1); j++) {
+                            System.out.print(" ");
+                        }
+                    }
+                } else {
+                    //Imprimira el espaciado y luego la frase
+                    for(int j = 0; j != (espaciado); j++) {
+                        System.out.print(" ");
+                    }
+                    
+                    System.out.print(frases[0]);
+
+                    //Imprime el resto de la linea
+                    for(int j = 0; j != (118 - frases[0].length() - espaciado); j++) {
+                        System.out.print(" ");
+                    }
+                }
+                
+            } else {
+
+                for(int j = 0; j != 118; j++) {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.print("=");
+            }
+
+        
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # VideojuegoFundamentos
 Es el proyecto de fundamentos de la programacion
+
+## FUNCINES
+La funcion paredes 
+si el espaciado es "-1", se centrara si no hara el espaciado que ponga 
