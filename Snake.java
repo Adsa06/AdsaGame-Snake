505b87e3f9e91211f99c4c83e546d6a167d0434b
Aitor de Santos Amoros <soyadsa06@gmail.com>
2024-10-30 10:05:22 +0100

    reformacion del snake

--- Snake.java
+++ Snake.java
@@ -2,35 +2,38 @@ public class Snake {
     public static void main(String[] args) throws InterruptedException {
         
         /* ----- Parte declarativa ----- */
-        char[] snake = {
-            '#',
-            '#'
-        };
-        int[] cords = {
-            10,
-            10
-        };
-        final int[] POSICION_INICIAL = {
-            10,
-            10
-        };
-        int posicion = 4;
+        
         boolean alive = true;
+        char[] snake = { '#' };
+        int snakeLongitud = 2;
+        int dimensiones[] = { 
+            20, 
+            20
+        };
+        int 
+        int posicion = 4;
         /* ----- Parte principal ----- */
-        //while (alive) {
-            sueloTecho(120);
+        while (alive) {
 
-            System.out.printf("%" + cords[0] +"s", snake[0]);
-            for(int i = 1; i < snake.length; i++) {
-                System.out.printf("%s", snake[i] , posicion);
+            //System.out.printf("%" + cords[0] +"s", snake[0]);
+
+
+            for(int i = 0; i < snakeLongitud; i++) {
+                System.out.printf("%" + posicion + "s", snake[0]);
             }
-            sueloTecho(120);
+
+
 
             //Tiempo de espera con hilos
+            separacion();
             Thread.sleep(1000);
-        //}
+        }
     }
-
+    public static void separacion() {
+        for(int i = 0; i < 10; i++) {
+            System.out.println("\n");
+        }
+    }
     public static void sueloTecho(int longitud) {
         System.out.printf("%n");
         for(int i = longitud; i > 0; i--) {
