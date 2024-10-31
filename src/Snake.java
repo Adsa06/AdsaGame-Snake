08653ffa1fd765931c053c06982f0821ab248426
Aitor de Santos Amoros <soyadsa06@gmail.com>
2024-10-31 21:33:04 +0100

    case S y W terminados, pero fallo en la parte de la cola en TODO

--- src/Snake.java
+++ src/Snake.java
@@ -35,17 +35,12 @@ public class Snake {
         //Cordenadas de la cabeza y de la cola
         //Primera cordenada la "x", posicion de una frase (empieza en 1) 
         //la segunda cordenada es una "y" la que indica que frase es (empieze en 0)
-        
-
-        int CordsCabeza[] = {5,5};
-        int CordsCola[] = {7,5};
-        /*  Pa el "D"
         int CordsCabeza[] = {7,5};
         int CordsCola[] = {5,5};
-        */
+        
 
         //Direccion
-        String direcion = "A";
+        String direcion = "S";
 
         //La sub cadenas para que se mueva
         String subCadenaCabeza1;
@@ -77,18 +72,17 @@ public class Snake {
             Thread.sleep(1000);
             switch (direcion) {
                 case "W":
-
                     /* ----- Parte de la cabeza ----- */
                     //Parto la cadena en 3 para sustituir un 0 en un 1, concretamente creo una nueva cabeza mas adelaneta gracias a la posicion de la cadena
-                    subCadenaCabeza1 = cordenadas2[CordsCabeza[1]].substring(0,CordsCabeza[0]);
-                    subCadenaCabeza2 = cordenadas2[CordsCabeza[1]].substring(CordsCabeza[0], CordsCabeza[0]+1);
+                    subCadenaCabeza1 = cordenadas2[CordsCabeza[1]-1].substring(0,CordsCabeza[0]-1);
+                    subCadenaCabeza2 = cordenadas2[CordsCabeza[1]-1].substring(CordsCabeza[0]-1, CordsCabeza[0]);
 
                     /* ------------------------------ Aqui para saber si se ha chocado tendria q detectar si es un 1 lo q se quiere remplazar ------------------------------*/
                     subCadenaCabeza2 = subCadenaCabeza2.replace("0","1");
 
-                    subCadenaCabeza3 = cordenadas2[CordsCabeza[1]].substring(CordsCabeza[0]+1);
+                    subCadenaCabeza3 = cordenadas2[CordsCabeza[1]-1].substring(CordsCabeza[0]);
                     //Renuevo la cadena utilizando la particion
-                    cordenadas2[CordsCabeza[1]] = subCadenaCabeza1 + subCadenaCabeza2 + subCadenaCabeza3;
+                    cordenadas2[CordsCabeza[1]-1] = subCadenaCabeza1 + subCadenaCabeza2 + subCadenaCabeza3;
 
                     /* ----- Parte de la cola ----- */
                     //Parto la cadena en 3 para sustituir un 0 en un 1, concretamente creo una nueva cola mas adelanete
@@ -136,15 +130,15 @@ public class Snake {
                 case "S":
                     /* ----- Parte de la cabeza ----- */
                     //Parto la cadena en 3 para sustituir un 0 en un 1, concretamente creo una nueva cabeza mas adelaneta gracias a la posicion de la cadena
-                    subCadenaCabeza1 = cordenadas2[CordsCabeza[1]].substring(0,CordsCabeza[0]);
-                    subCadenaCabeza2 = cordenadas2[CordsCabeza[1]].substring(CordsCabeza[0], CordsCabeza[0]+1);
+                    subCadenaCabeza1 = cordenadas2[CordsCabeza[1]+1].substring(0,CordsCabeza[0]-1);
+                    subCadenaCabeza2 = cordenadas2[CordsCabeza[1]+1].substring(CordsCabeza[0]-1, CordsCabeza[0]);
                 
                     /* ------------------------------ Aqui para saber si se ha chocado tendria q detectar si es un 1 lo q se quiere remplazar ------------------------------*/
                     subCadenaCabeza2 = subCadenaCabeza2.replace("0","1");
 
-                    subCadenaCabeza3 = cordenadas2[CordsCabeza[1]].substring(CordsCabeza[0]+1);
+                    subCadenaCabeza3 = cordenadas2[CordsCabeza[1]+1].substring(CordsCabeza[0]);
                     //Renuevo la cadena utilizando la particion
-                    cordenadas2[CordsCabeza[1]] = subCadenaCabeza1 + subCadenaCabeza2 + subCadenaCabeza3;
+                    cordenadas2[CordsCabeza[1]+1] = subCadenaCabeza1 + subCadenaCabeza2 + subCadenaCabeza3;
 
                     /* ----- Parte de la cola ----- */
                     //Parto la cadena en 3 para sustituir un 0 en un 1, concretamente creo una nueva cola mas adelanete
