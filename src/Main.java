602fcaa4f395cdc60dc5db55c83d91aecccf08a4
Aitor de Santos Amoros <soyadsa06@gmail.com>
2024-12-09 12:48:12 +0100

    implementacion de admite colores en el main

--- src/Main.java
+++ src/Main.java
@@ -33,13 +33,23 @@ public class Main {
             "2 = Confi",
             "3 = Salir"
         };
-        String[] explicacionJuego = {
-            "Paso 1, Abra el Controlador del juego para poder escribir las direciones por consola, si desea salir del controlador pon \"Salir\"",
-            "Paso 2, Muevete con " + ColoresConsola.ANSI_CYAN() + "\"W\"" + ColoresConsola.ANSI_RESET() + "," + ColoresConsola.ANSI_CYAN() + "\"A\"" + ColoresConsola.ANSI_RESET() + "," + ColoresConsola.ANSI_CYAN() + "\"S\"" + ColoresConsola.ANSI_RESET() + "," + ColoresConsola.ANSI_CYAN() + "\"D\"" + ColoresConsola.ANSI_RESET() + ", Arriba, izquierda, abajo y derecha respectivamente",
-            "Las letras tendras q escribirlas en el programa controlador y tendras que darle " + ColoresConsola.ANSI_CYAN() + "Enter/Intro" + ColoresConsola.ANSI_RESET() + " para enviarlas, " + ColoresConsola.ANSI_CYAN() + "de una en una" + ColoresConsola.ANSI_RESET(),
-            "Paso 3, Evita chocarte con las paredes y tu cuerpo, a su vez evita ir a la direcion contraria a la que vas",
-            "Paso 4, Presiona la tecla " + ColoresConsola.ANSI_UNDERLINE() + "enter" + ColoresConsola.ANSI_RESET() + " para empezar a jugar",
-            "Recuerda cerrar el controlador principal al acabar de jugar, para cerrarlo introduzca \"" + ColoresConsola.ANSI_UNDERLINE() + "Salir" + ColoresConsola.ANSI_RESET() + "\""
+        String[][] explicacionJuego = {
+            {
+                "Paso 1, Abra el Controlador del juego para poder escribir las direciones por consola, si desea salir del controlador pon \"Salir\"",
+                "Paso 2, Muevete con " + ColoresConsola.ANSI_CYAN() + "\"W\"" + ColoresConsola.ANSI_RESET() + "," + ColoresConsola.ANSI_CYAN() + "\"A\"" + ColoresConsola.ANSI_RESET() + "," + ColoresConsola.ANSI_CYAN() + "\"S\"" + ColoresConsola.ANSI_RESET() + "," + ColoresConsola.ANSI_CYAN() + "\"D\"" + ColoresConsola.ANSI_RESET() + ", Arriba, Izquierda, Abajo y Derecha respectivamente",
+                "Las letras tendras q escribirlas en el programa controlador y tendras que darle " + ColoresConsola.ANSI_CYAN() + "Enter/Intro" + ColoresConsola.ANSI_RESET() + " para enviarlas, " + ColoresConsola.ANSI_CYAN() + "de una en una" + ColoresConsola.ANSI_RESET(),
+                "Paso 3, Evita chocarte con las paredes y tu cuerpo, a su vez evita ir a la direcion contraria a la que vas",
+                "Paso 4, Presiona la tecla " + ColoresConsola.ANSI_UNDERLINE() + "enter" + ColoresConsola.ANSI_RESET() + " para empezar a jugar",
+                "Recuerda cerrar el controlador principal al acabar de jugar, para cerrarlo introduzca \"" + ColoresConsola.ANSI_UNDERLINE() + "Salir" + ColoresConsola.ANSI_RESET() + "\""
+            },
+            {
+                "Paso 1, Abra el Controlador del juego para poder escribir las direciones por consola, si desea salir del controlador pon \"Salir\"",
+                "Paso 2, Muevete con \"W\", \"A\", \"S\", \"D\", Arriba, izquierda, abajo y derecha respectivamente",
+                "Las letras tendras q escribirlas en el programa controlador y tendras que darle Enter/Intro para enviarlas, de una en una",
+                "Paso 3, Evita chocarte con las paredes y tu cuerpo, a su vez evita ir a la direcion contraria a la que vas",
+                "Paso 4, Presiona la tecla enter para empezar a jugar",
+                "Recuerda cerrar el controlador principal al acabar de jugar, para cerrarlo introduzca \"Salir\""
+            },
         };
 
         final int RESOLUCION_ALTURA = 30;
@@ -82,7 +92,7 @@ public class Main {
                     BufferedWriter fr = new BufferedWriter(new FileWriter("./content.txt", false));
                     fr.close();
                     /* ----- */
-                    for(String frases : explicacionJuego) {
+                    for(String frases : explicacionJuego[admiteColores]) {
                         System.out.println(frases);
                     }
                     //Esto hace q no continue el programa sin que presione el enter
